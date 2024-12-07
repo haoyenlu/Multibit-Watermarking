c83c6503a03c25f39edc8595e46c333335e7f073
haoenlu <haoenlu.07@gmail.com>
2024-12-07 16:08:17 -0500

    update

--- src/success_rate.py
+++ src/success_rate.py
@@ -95,7 +95,7 @@ def main():
         score_dict = mb_watermark_detector.detect(output_text, return_scores=False)
         print(score_dict)
 
-        tn, fp, fn, tp = confusion_matrix(list(message_binary), list(score_dict['predict_message'])).raevl()
+        tn, fp, fn, tp = confusion_matrix(list(message_binary), list(score_dict['predict_message'])).ravel()
         accuracy.append((tn + tp) /(tn + fp + fn + tp))
 
         total_cnt += 1
